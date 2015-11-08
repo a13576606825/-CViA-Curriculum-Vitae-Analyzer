24eceb2beacf58254ff7653e6b2e072105f4bfbb
Zhang Ji <zhangji951027@gmail.com>
2015-11-08 21:32:49 +0800

    remove aredundant lines

--- src/logic/OpenFolder.java
+++ src/logic/OpenFolder.java
@@ -15,23 +15,6 @@ public class OpenFolder {
 	private static boolean isMac() {
 		return (OS.indexOf("mac") >= 0);
 	}
-	
-	private static final String[] prefix = {
-	      "osascript",
-	      "-e", "tell application \"Finder\"",
-	      "-e", "activate",
-	      "-e", "<openCmdGoesHere>",
-	      "-e", "end tell"
-	  };
-	 
-	  private static String buildFolderCommand(String folderPath) {
-	    StringBuilder openCommand = new StringBuilder("open ");
-	    String[] pathParts = folderPath.split("/");
-	    for (int i = pathParts.length - 1; i > 0; i--) {
-	      openCommand.append("folder \"").append(pathParts[i]).append("\" of ");
-	    }
-	    return openCommand.append("startup disk").toString();
-	  }
 
 	
 	public static void openFolder(String path) {
@@ -52,14 +35,7 @@ public class OpenFolder {
 	}
 	 
 	public static void main(String[] args) {
-//		openFolder( new File("/Users/JJ/Desktop/File Interchange").getAbsolutePath());
-//		prefix[6] = buildFolderCommand("/Users/JJ/Desktop/CS4211 Final Presentation.key/");
-//		try {
-//			Runtime.getRuntime().exec(prefix).waitFor();
-//		} catch (InterruptedException | IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
+
 		openFolder("/Users/JJ/Desktop/CS4211\\ Final\\ Presentation.key");
 	}
 }
